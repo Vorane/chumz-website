527df91eca3db8e1a59ab168d4b86fe6c2603455
Evans Munene <evans@vorane.com>
2021-08-11 15:49:23 +0300

    TCW update privacy policy

--- src/Pages/Policy/Policy.js
+++ src/Pages/Policy/Policy.js
@@ -4,64 +4,108 @@ import styled from 'styled-components'
 const BoldText = styled.strong`
 	color: #3d3d3d;
 `
+const Body = styled.div`
+	& p,
+	li {
+		color: black !important;
+	}
+`
 
 const Policy = () => {
 	return (
-		<div>
+		<Body>
 			<div className='container'>
 				<div className='row text-left justify-content-left'>
 					<section className='fdb-block'>
-						<h1 id='untitled'>Privacy Policy</h1>
+						
+						<h1 id='-privacy-policy-'>
+							<strong>Privacy Policy</strong>
+						</h1>
 						<p>
 							This Privacy Policy explains how Chumz uses your
 							Personal Data collected or processed from you in
 							providing a savings app facility for saving money
 							for self or others (“Service”).
 						</p>
-						<h2 id='-who-processes-your-personal-data-'>
-							<strong>Who processes your Personal Data?</strong>
-						</h2>
 						<p>
-							Chumz processes and stores Personal Data collected
-							from you via your use of the Service or where you
-							have given your consent.
-						</p>
-						<h2 id='-we-might-ask-you-to-provide-us-with-personal-data-'>
 							<strong>
-								We might ask you to provide us with Personal
-								Data
+								Collecting Personal Data- What we collect and
+								Use
 							</strong>
+						</p>
+						<p>Chumz collects data in two ways:</p>
+						<p>
+							1) The data you submit when you register in the
+							application. This data is composed of personally
+							identifiable information such as: Identification
+							number, date of birth, phone number, gender and age
+						</p>
+						<p>
+							2) Data collected through the app permissions: Chumz
+							requests the user to give the app permission to
+							enable access of the users contact list and
+							messages. Once the permissions are given the app is
+							able to collect contact list information. This
+							information enables a better user experience in
+							inviting members to the app from a central service
+							the app can access with your data and the messages
+							enables the app advise you on how to invest based on
+							your spending habits
+						</p>
+						<p>
+							( You can check the section on how we use your
+							personal data under this Privacy Policy to get more
+							information on how the data is used)
+						</p>
+						<h2 id='-processing-your-personal-data-'>
+							<strong>Processing your Personal Data</strong>
 						</h2>
 						<p>
-							Chumz may request that you provide Personal Data
-							when offering the Service to you. This Personal Data
-							may include contact details, copies of
-							identification documentation derived from publicly
-							accessible databases, your national identity number
-							as well as information relating to your device or
-							Internet service (such as an IP address and a MAC
-							number).
+							Chumz processes the data and stores it in Google
+							cloud servers. The Google cloud services offer us
+							hosting solutions to enable processing of the data
+							and storing of the information generated from the
+							data.
+						</p>
+						<p>
+							We also use third party data analysis tool called
+							metabase to assist in data analytics of the users
+							data
+						</p>
+						<h2 id='-information-collection-and-use-'>
+							<strong>Information Collection and Use</strong>
+						</h2>
+						<p>
+							For a better experience, while using our Service, we
+							may require you to provide us with your contact list
+							information, messages and transaction data. The
+							information that we request will be retained by us
+							and used as described in this privacy policy.
+						</p>
+						<p>
+							The app does use third party services that may
+							collect information used to identify you or to
+							enable a better experience.
 						</p>
 						<h2 id='-how-we-use-your-personal-data-'>
 							<strong>How we use your Personal Data</strong>
 						</h2>
 						<p>
-							<BoldText>Contact list information:</BoldText> We
-							request permission to access your contact list
-							information to enable you invite friends and family
-							to join your financial investment groups from the
-							app to your phone-book contact list. By clicking
-							invite a member from the app a link is sent to the
-							person to join your group via the the contact list
-							permission you have authorized. You can opt-in or
-							opt-out of the contact list information access
-							feature.
+							Contact list information: We request permission to
+							access your contact list information to enable you
+							to invite friends and family to join your financial
+							investment groups from the app to your phone-book
+							contact list. By clicking invite a member from the
+							app a link is sent to the person to join your group
+							via the contact list permission you have authorized.
+							You can opt-in or opt-out of the contact list
+							information access feature.
 						</p>
 						<p>
-							<BoldText>Messages:</BoldText> To enable Chumz guide
-							and advise you on how to save based on your mobile
-							money financial transactions. This option can be
-							switched on and off in the app
+							Messages: To enable Chumz guide and advise you on
+							how to save based on your mobile money financial
+							transactions. This option can be switched on and off
+							in the app
 						</p>
 						<p>
 							Chumz uses Personal Data to communicate with you and
@@ -95,10 +139,10 @@ const Policy = () => {
 						</p>
 						<ul>
 							<li>
-								If we think that sharing it is necessary to
-								enforce the terms of a contract with you or to
-								protect the rights, property or safety of Chumz
-								or the services provided by Chumz.
+								If we think that sharing is necessary to enforce
+								the terms of a contract with you or to protect
+								the rights, property or safety of Chumz or the
+								services provided by Chumz.
 							</li>
 							<li>
 								To enable us to comply with the law or to
@@ -115,7 +159,7 @@ const Policy = () => {
 							<li>
 								If you request a service or product that is
 								administered by a third party, in order to allow
-								us to respond to the request. If such third
+								us to respond to the request. If such a third
 								party is working for Chumz it may also send back
 								to Chumz any new information obtained from you.
 							</li>
@@ -131,12 +175,12 @@ const Policy = () => {
 							</li>
 							<li>
 								To third parties who provide services to Chumz
-								(such as such as administration or technical
-								services). However we ensure that such partners
-								do not use Personal Data for any other purposes,
-								do not disclose it to anyone else and that they
-								do not retain copies except as necessary to
-								provide services to us or as required by law.
+								(such as administration or technical services).
+								However we ensure that such partners do not use
+								Personal Data for any other purposes, do not
+								disclose it to anyone else and that they do not
+								retain copies except as necessary to provide
+								services to us or as required by law.
 							</li>
 						</ul>
 						<h2 id='-storage-security-and-retention-of-your-personal-data-'>
@@ -147,11 +191,10 @@ const Policy = () => {
 						</h2>
 						<p>
 							Personal Data that we collect is stored on our
-							servers which may be located outside Kenya and may
-							also be processed by staff operating outside the
-							Kenya. We implement and maintain the highest levels
-							of security to protect any information stored on our
-							servers and to comply with all local rules.
+							Google Hosting Service. We implement and maintain
+							the highest levels of security to protect any
+							information stored on our servers and to comply with
+							all local rules and global GDPR rules.
 						</p>
 						<p>
 							It is your responsibility to keep your password
@@ -193,7 +236,11 @@ const Policy = () => {
 							or the use of your Personal Data, please contact us
 							by sending an email to the following address
 							(indicating “PRIVACY REQUEST” in the message line:
-							privacy@chumz.io
+						</p>
+						<p>
+							<a href='mailto:sam@moneto.co.ke'>
+								sam@moneto.co.ke
+							</a>
 						</p>
 						<p>
 							If you contact us by email, we may keep a record of
@@ -232,7 +279,7 @@ const Policy = () => {
 					</section>
 				</div>
 			</div>
-		</div>
+		</Body>
 	)
 }
 export default Policy
